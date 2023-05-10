42a41849338de31bc0af8534371ca7894ead8708
Mohamed Sghir <med.sg125@gmail.com>
2023-05-10 12:30:47 +0100

    first version

---
+++ components/footer.php
@@ -0,0 +1,35 @@
+<footer class="footer">
+
+   <section class="grid">
+
+      <div class="box">
+         <h3>quick links</h3>
+         <a href="home.php"> <i class="fas fa-angle-right"></i> Acceuil</a>
+         <a href="about.php"> <i class="fas fa-angle-right"></i> A propos de</a>
+         <a href="shop.php"> <i class="fas fa-angle-right"></i> Voir les médicaments</a>
+         <a href="contact.php"> <i class="fas fa-angle-right"></i> contact</a>
+      </div>
+
+      <div class="box">
+         <h3>extra links</h3>
+         <a href="user_login.php"> <i class="fas fa-angle-right"></i> Se connecter</a>
+         <a href="user_register.php"> <i class="fas fa-angle-right"></i> S'inscire</a>
+         <a href="cart.php"> <i class="fas fa-angle-right"></i> Panier</a>
+         <a href="orders.php"> <i class="fas fa-angle-right"></i> Commande</a>
+      </div>
+
+      <div class="box">
+         <h3>contactez nous </h3>
+         <a href="tel:1234567890"><i class="fas fa-phone"></i>0678389281</a>
+         <a href="tel:11122233333"><i class="fas fa-phone"></i>0627482035</a>
+         <a href="mailto:shaikh@gmail.com"><i class="fas fa-envelope"></i> SOPHACHARK@gmail.com</a>
+         <a href="https://www.google.com/myplace"><i class="fas fa-map-marker-alt"></i> OUJDA , Maroc</a>
+      </div>
+
+      
+
+   </section>
+
+   <div class="credit">&copy; copyright @ <?= date('Y'); ?> by <span>SOPHA CHARK</span> | all rights reserved!</div>
+
+</footer>
